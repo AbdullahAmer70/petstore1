298be99872353c54016b7358e0fd47fdd2cb6008
AbdullahAmer70 <animedits213@gmail.com>
2024-05-06 15:22:10 +0500

    CM1 from division.

--- calculator.sh
+++ calculator.sh
@@ -13,3 +13,11 @@ multiply() {
     echo "Result of multiplication: $result"
 }
 
+divide() {
+    if [ $2 -eq 0 ]; then
+        echo "Error: Division by zero is not allowed."
+    else
+        result=$(echo "scale=2; $1 / $2" | bc)
+        echo "Result of division: $result"
+    fi
+}
